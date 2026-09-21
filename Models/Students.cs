7795b98f5a98a46bff71e80686b0d826f8156c0a
Giorgi <giorgibucxrikidze857@gmail.com>
2026-09-21 18:14:47 +0400

    add student registration menu

---
+++ Models/Students.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace StudentReg.Models
+{
+    internal class Students
+    {
+        public string? Name { get; set; }
+        public long Id { get; set; }
+        public decimal ? Gpi { get; set; }
+        public string? Email { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,43 @@
-﻿namespace StudentReg
+﻿using StudentReg.Models;
+
+namespace StudentReg
 {
-class Program
+    class Program
     {
+        public static List<Students> AllStudent = new List<Students>();
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("Student Registration");
+                Console.WriteLine("1. Register New Student");
+                Console.WriteLine("2. Edit Your Information");
+                Console.WriteLine("3. Check Your Status");
+                Console.WriteLine("4. Exit");
 
+                Console.WriteLine("\nChoose:");
+                string? opn;
+                opn = Console.ReadLine();
+
+                switch (opn)
+                {
+                    case "1": // registration
+
+                        break;
+                    case "2":
+
+                        break;
+                        
+                    case "3":
+
+                        break;
+
+                    case "4":
+
+                        break;
+                }
+
+            }
         }
     }
 }
